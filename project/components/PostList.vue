d69292c4b5987d2b7417879ba7125828f84f7bbe
Max <ppmax@bk.ru>
2023-05-16 00:04:41 +0300

    Refactoring with PostItem

--- project/components/PostList.vue
+++ project/components/PostList.vue
@@ -1,12 +1,20 @@
 <template>
-  <div class="post" v-for="post in posts" v-bind:key="post.id">
-    <div><strong>Название: </strong> {{ post.title }}</div>
-    <div><strong>Описание: </strong> {{ post.body }}</div>
+  <div>
+    <h3>Список пользователей</h3>
+    <post-item
+      v-for="post in posts"
+      :post="post"
+      :key="post.id" />
   </div>
 </template>
 
 <script>
+import PostItem from './PostItem.vue';
+
 export default {
+  components: {
+    PostItem,
+  },
   props: {
     posts: {
       type: Array,
@@ -17,9 +25,5 @@ export default {
 </script>
 
 <style scoped>
-.post {
-  margin-top: 15px;
-  padding: 15px;
-  border: 2px solid teal;
-}
+
 </style>
