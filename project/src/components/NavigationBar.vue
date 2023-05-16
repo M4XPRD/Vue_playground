6d8f0ce695c2893c3862ecd8b117101c97d5a5a3
Max <ppmax@bk.ru>
2023-05-17 02:07:06 +0300

    Navbar created

---
+++ project/src/components/NavigationBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="navbar">
+    <div @click="$router.push('/')">Vue3</div>
+    <div class="navbar-buttons">
+      <my-button @click="$router.push('/posts')">Пользователи</my-button>
+      <my-button @click="$router.push('/about')" style="margin-left: 20px;">О сайте</my-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style scoped>
+  .navbar {
+    height: 50px;
+    background-color: lightcyan;
+    box-shadow: 2px 2px 4px gray;
+    display: flex;
+    align-items: center;
+    padding: 0 15px;
+  }
+
+  .navbar-buttons  {
+    margin-left: auto;
+
+  }
+</style>
