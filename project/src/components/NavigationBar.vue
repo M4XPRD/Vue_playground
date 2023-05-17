7f797686ba80d178525d786d1e073d302b345b2f
Max <ppmax@bk.ru>
2023-05-18 00:37:36 +0300

    Navigation updated

--- project/src/components/NavigationBar.vue
+++ project/src/components/NavigationBar.vue
@@ -4,6 +4,8 @@
     <div class="navbar-buttons">
       <my-button @click="$router.push('/posts')">Посты</my-button>
       <my-button @click="$router.push('/about')" style="margin-left: 20px;">О сайте</my-button>
+      <my-button @click="$router.push('/store')" style="margin-left: 20px;">Состояние</my-button>
+      <my-button @click="$router.push('/composition')" style="margin-left: 20px;">Композиция</my-button>
     </div>
   </div>
 </template>
