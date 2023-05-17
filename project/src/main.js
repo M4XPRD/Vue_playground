495b95ce7355c198e7fbf38ba12c9d09f1cd5866
Max <ppmax@bk.ru>
2023-05-17 03:15:49 +0300

    New directives created

--- project/src/main.js
+++ project/src/main.js
@@ -2,11 +2,16 @@ import { createApp } from 'vue';
 import components from './components/UI';
 import App from './App.vue';
 import router from './routes/router';
+import directives from './directives/index';
 
 const app = createApp(App);
 
 components.forEach((component) => {
   app.component(component.name, component);
+});
+
+directives.forEach((directive) => {
+  app.directive(directive.name, directive);
 });
 
 app
